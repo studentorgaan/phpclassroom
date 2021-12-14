5dc983f7c11a2c8b67121fe692355242bc8cd9fc
studentorgaan <89797336+studentorgaan@users.noreply.github.com>
2021-12-14 09:46:48 +0700

    Update province.php

--- province.php
+++ province.php
@@ -4,7 +4,7 @@ $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-
 $data = json_decode($json);
 
   foreach($data as $key=>$val){
-    echo $val -> province."<br>";
+    echo $val -> province." : ".$val->new_case."<br>";
   }
   
   ?>
